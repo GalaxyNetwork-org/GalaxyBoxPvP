afc7fbf33b3c7a42aca83df9de820c8338bdaf7e
Lncvrt <lncvrtreal@gmail.com>
2024-07-12 22:20:24 -0700

    Tweak BlockPlaceListener.java

--- src/main/java/io/github/lncvrt/lncvrtbox/events/BlockPlaceListener.java
+++ src/main/java/io/github/lncvrt/lncvrtbox/events/BlockPlaceListener.java
@@ -20,28 +20,20 @@ public class BlockPlaceListener implements Listener {
                 String displayName = ChatColor.stripColor(event.getItemInHand().getItemMeta().getDisplayName());
                 if (displayName.equalsIgnoreCase("Coal Fragment")) {
                     event.setCancelled(true);
-                    return; // Exit early if placing a Coal Fragment button
                 }
             }
-        } else if (event.getBlockPlaced().getType() == Material.FURNACE ||
-                event.getBlockPlaced().getType() == Material.FURNACE_MINECART ||
-                event.getBlockPlaced().getType() == Material.BLAST_FURNACE) {
+        } else if (event.getBlockPlaced().getType() == Material.FURNACE || event.getBlockPlaced().getType() == Material.FURNACE_MINECART || event.getBlockPlaced().getType() == Material.BLAST_FURNACE) {
             Player player = event.getPlayer();
             String message = String.format("%s%s[TIP]%s%s If you are trying to smelt iron, gold, etc you can use the Smelter Shop. Click %s[HERE]%s to teleport to the smelter shop! (if you are on bedrock, instead of clicking there run %s/warp smelter%s.", GREEN, BOLD, RESET, GREEN, UNDERLINE, GREEN, UNDERLINE, GREEN);
 
-            // Create the entire message as a TextComponent
-            TextComponent fullMessage = new TextComponent(TextComponent.fromLegacyText(message));
-
-            // Find the starting index of [HERE] in the message
+            TextComponent fullMessage;
             int hereIndex = message.indexOf("[HERE]");
 
-            // Create a TextComponent just for the [HERE] part
             TextComponent clickableHere = new TextComponent("[HERE]");
             clickableHere.setColor(net.md_5.bungee.api.ChatColor.GREEN);
             clickableHere.setUnderlined(true);
             clickableHere.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/warp smelter"));
 
-            // Replace [HERE] in the full message with the clickable TextComponent
             TextComponent[] components = new TextComponent[] {
                     new TextComponent(message.substring(0, hereIndex)),
                     clickableHere,
@@ -49,11 +41,7 @@ public class BlockPlaceListener implements Listener {
             };
             fullMessage = new TextComponent(components);
 
-            // Send the modified message to the player
             player.spigot().sendMessage(fullMessage);
-        } else if (event.getBlockPlaced().getType() == Material.WITHER_SKELETON_SKULL) {
-            event.setCancelled(true);
-            event.getPlayer().getInventory().remove(Material.WITHER_SKELETON_SKULL);
         }
     }
 }
